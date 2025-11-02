b7ee9b7522c26a050a03832444e809202b8a5d2d
BhavyashreeKP <bhavyashreekp11@gmail.com>
2025-11-02 10:25:55 +0530

    dummy change

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,6 +1,6 @@
 import dp from '../assets/png/my.jpg'
 export const headerData = {
-    name: 'Bhavyashree K P',
+    name: 'Bhavyashreeeeeeeeeeeeeeeeeeeeeeeeeeeeeee K P',
     title: "Hello! I am",
     desciption: "Hello! I'm Bhavyashree K P, a passionate MERN stack developer specializing in MongoDB, Express.js, React.js, and Node.js. I thrive in fast-paced environments, enjoy tackling new challenges, and delivering thoughtful, scalable solutions. Driven by curiosity, I continually learn new technologies to expand my skill set and create meaningful, high-impact web applications.",
     image: dp,
